46555541f7c4699b9c36990b824536011710c753
Cody <cody.ning@ubc.ca>
2022-04-02 03:02:23 -0700

    server with NLP model

---
+++ Server/main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, Request
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from model import QA
+
+app = FastAPI()
+
+@app.get("/", status_code=200)
+async def root():
+    return {"yey": "asd"}
+
+@app.post("/CV/getLabel")
+async def get_label(file: UploadFile):
+    image = np.array(Image.open(BytesIO(await file.read())))
+    image_str = np.array2string(image, precision=2, separator=',',
+                      suppress_small=True)
+    return {"label": image_str}
+
+@app.post("/NLP/getResponse")
+async def get_response(req: Request):
+    body = await req.json()
+    answer =  QA(body['question'])
+    return {"answer": answer}
+
+if __name__ == "__main__":
+    uvicorn.run(app,
+        host="0.0.0.0",
+        port=8888,
+        debug='true')
---
+++ Server/model.py
@@ -0,0 +1,32 @@
+import argparse
+import json
+import os
+from collections import OrderedDict
+import torch
+import csv
+import util
+from transformers import BertTokenizer, BertForQuestionAnswering
+
+
+def QA(passage):
+    # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = "cuda:0"
+    checkpoint_path = os.path.join('./model/checkpoint')
+    model = BertForQuestionAnswering.from_pretrained(checkpoint_path)
+    tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    model.to(device)
+    model.eval()
+    question = "what do you think?"
+    # passage = "My name is pepper."
+    input_ids = tokenizer.encode(question, passage)
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    sep_index = input_ids.index(tokenizer.sep_token_id)
+    num_seg_a = sep_index + 1
+    num_seg_b = len(input_ids) - num_seg_a
+    segment_ids = [0]*num_seg_a + [1]*num_seg_b
+    assert len(segment_ids) == len(input_ids)
+    start_scores, end_scores = model(torch.tensor([input_ids]).to(device), token_type_ids=torch.tensor([segment_ids]).to(device), return_dict=False)
+    answer_start = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores)
+    answer = ' '.join(tokens[answer_start:answer_end+1])
+    return answer
